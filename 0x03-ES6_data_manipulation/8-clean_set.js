c5ec1941d365bab541a7abdf759b058ecd3593f3
Moahmed Elafandy <mohamedelafandy593@gmail.com>
2024-04-16 22:37:18 +0000

    update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,22 +1,20 @@
 export default function cleanSet(set, startString) {
   if (
-    !set &&
-    !startString &&
-    !(set instanceof Set) &&
-    typeof startString !== "string") {
-    return "";
+    !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
+  ) {
+    return '';
   }
 
-  const arr = [];
+  const parts = [];
 
   for (const value of set.values()) {
-    if (typeof value === "string" && value.startsWith(startString)) {
-      const newValue = value.substring(startString.length);
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
 
-      if (newValue && newValue !== value) {
-        arr.push(newValue);
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
       }
     }
   }
-  return arr.join("-");
+  return parts.join('-');
 }
