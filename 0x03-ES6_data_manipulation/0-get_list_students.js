97cd3a2292519aa3e13f6b99570c4514395350a5
Moahmed Elafandy <mohamedelafandy593@gmail.com>
2024-04-16 18:36:22 +0000

    update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,20 @@
-const getListStudents = export default function getListStudents() {
-  return [
-    {
-      id: 1,
-      firstName: "Guillaume",
-      location: "San Francisco",
-    },
-    {
-      id: 2,
-      firstName: "James",
-      location: "Columbia",
-    },
-    {
-      id: 5,
-      firstName: "Serena",
-      location: "San Francisco",
-    },
-  ];
-}
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: "Guillaume",
+    location: "San Francisco",
+  },
+  {
+    id: 2,
+    firstName: "James",
+    location: "Columbia",
+  },
+  {
+    id: 5,
+    firstName: "Serena",
+    location: "San Francisco",
+  },
+];
+
+export default getListStudents;
+
