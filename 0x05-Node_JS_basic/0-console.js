8c8d3b18ac94ea1fa6a056882e5bdce73979fbfc
unknown <mohamedelafandy593@gmail.com>
2024-06-06 08:42:06 -0700

    console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,17 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function
+
+const displayMessage = (STDOUT) => console.log(STDOUT);
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
+
+module.exports = displayMessage;
