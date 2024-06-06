f090c5ff1429dfaed77a9b85e25eb62c3072645c
unknown <mohamedelafandy593@gmail.com>
2024-06-06 08:57:11 -0700

    update

--- 0x05-Node_JS_basic/0-console.js
+++
@@ -1,8 +0,0 @@
-/**
- * Logs a message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
- */
-const displayMessage = (STDOUT) => console.log(STDOUT);
-
-module.exports = displayMessage;
